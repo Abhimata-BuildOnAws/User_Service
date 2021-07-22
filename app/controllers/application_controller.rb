d2ae31e06afa95e2d3c3bc390f403c7697a2124d
Gary Peabody <aikarvin99@gmail.com>
2021-07-22 14:28:04 +0800

    Bugfix: end of input for health_check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
   def health_check
     render json: { message: 'User Service is healthy' }, status:200
+  end
 
   def current_user(params)
     User.find(params[:user_id])
