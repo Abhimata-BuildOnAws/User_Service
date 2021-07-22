4eb142f90ffd91327786631fa027e28a76765ca5
Gary Peabody <aikarvin99@gmail.com>
2021-07-22 14:25:48 +0800

    Add health check to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/health_check', to: 'application#health_check'
   get '/current_location', to: 'application#current_location'
   post '/browse', to: 'user#browse'
   post '/user/create', to: 'user#create_user'
