ebf6543bce9ba1e5ca78632032b319bef9518f1f
Gary Peabody <aikarvin99@gmail.com>
2021-07-22 14:24:25 +0800

    Add health check

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :null_session
+  def health_check
+    render json: { message: 'User Service is healthy' }, status:200
+
   def current_user(params)
     User.find(params[:user_id])
   end
