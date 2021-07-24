92fea2a87d43760209568af03b0efff9c6f53280
Gary Peabody <aikarvin99@gmail.com>
2021-07-24 16:21:00 +0800

    Remove redundant methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,12 +7,4 @@ class ApplicationController < ActionController::Base
   def current_user(params)
     User.find(params[:user_id])
   end
-
-  def current_location
-    ip = request.remote_ip
-    results = Geocoder.search(ip)
-    render json:{ ip: ip,
-                  coordinates: results.first.coordinates 
-                }
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  get '/health_check', to: 'application#health_check'
-  get '/current_location', to: 'application#current_location'
-  post '/browse', to: 'user#browse'
+  get '/user/health_check', to: 'application#health_check'
   post '/user/create', to: 'user#create_user'
   post '/user/get_user', to: 'user#get_user'
   post '/user/update_tree_points', to: 'user#update_tree_points'
